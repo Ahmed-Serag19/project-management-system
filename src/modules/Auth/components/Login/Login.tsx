53bb6f25b0e5a1103b2971176b4e183dcc6df106
Ahmed Mohamed <ahmedmohamed.amin@hotmail.com>
2024-08-22 15:32:10 +0300

    used password and email validations from the constants files

--- src/modules/Auth/components/Login/Login.tsx
+++ src/modules/Auth/components/Login/Login.tsx
@@ -9,6 +9,10 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { User_URls } from '../../../../constants/End_Points';
+import {
+  EmailValidation,
+  PasswordValidation,
+} from '../../../../constants/Validations';
 
 type LoginFormInputs = {
   email: string;
@@ -34,7 +38,7 @@ const LoginForm: React.FC = () => {
       const response = await axios.post(User_URls.login, data);
 
       toast.success('Logged in successfully!', {
-        position: 'top-center',
+        position: 'top-right',
         autoClose: 3000,
         hideProgressBar: false,
         closeOnClick: true,
@@ -94,14 +98,7 @@ const LoginForm: React.FC = () => {
           <Form.Control
             type="email"
             placeholder="Enter your E-mail"
-            {...register('email', {
-              required: 'E-mail is required',
-              pattern: {
-                value:
-                  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                message: 'Enter a valid e-mail address',
-              },
-            })}
+            {...register('email', EmailValidation)}
             isInvalid={!!errors.email}
           />
           <Form.Control.Feedback type="invalid">
@@ -118,15 +115,7 @@ const LoginForm: React.FC = () => {
             <Form.Control
               type={showPassword ? 'text' : 'password'}
               placeholder="Enter your password"
-              {...register('password', {
-                required: 'Password is required',
-                pattern: {
-                  value:
-                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
-                  message:
-                    'Password must be at least 6 characters long, and include at least one uppercase letter, one lowercase letter, one number, and one special character',
-                },
-              })}
+              {...register('password', PasswordValidation)}
               isInvalid={!!errors.password}
             />
             <InputGroup.Text onClick={togglePasswordVisibility}>
