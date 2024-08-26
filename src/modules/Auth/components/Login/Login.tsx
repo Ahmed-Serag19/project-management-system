167d1925321b68220d6ff29c22902d32237a5012
Ahmed-Serag19 <ahmedmohamed.amin@hotmail.com>
2024-08-26 10:22:09 +0300

    Modified unused imports

--- src/modules/Auth/components/Login/Login.tsx
+++ src/modules/Auth/components/Login/Login.tsx
@@ -42,8 +42,6 @@ const LoginForm: React.FC = () => {
         data
       );
       toast.success("Logged in successfully!");
-
-      toast.success("Logged in successfully!");
       saveToken(response.data.token);
       localStorage.setItem("token", response.data.token);
 
